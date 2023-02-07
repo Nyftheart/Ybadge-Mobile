1ed9723b45d576ac42a62525bc3e3b847fa59797
nyftheart <alexandre.kramer@ynov.com>
2023-02-07 13:58:38 +0100

    update design

--- pages/PageBadgesCompte.tsx
+++ pages/PageBadgesCompte.tsx
@@ -6,13 +6,23 @@ export default function PageBadgesCompte() {
     return (
 
         <div className={"bg-Background min-h-screen"} >
-            <div className={"text-center pt-7"}>
+
+            <div className={"text-center pt-16"}>
+                <Link href="/PageBadgesProfilBadges">
+                    <img
+                        src='/arrow.png'
+                        width={21}
+                        height={14}
+                        alt='logo'
+                        className=' ml-6'
+                    />
+                </Link>
                 <img
                     src='/Recherche_Logo_Ybadges.png'
                     width={125}
                     height={100}
                     alt='logo'
-                    className='m-auto pt-10 pb-8'
+                    className='m-auto  pb-8'
                 />
             </div>
             <div className={"text-Text text-red-600 text-3xl flex items-center mt-10"}>
@@ -28,32 +38,27 @@ export default function PageBadgesCompte() {
             </div>
 
             <div className={"text-Text text-blue-500 ml-32 mt-8 "}>
-                <div>NOM :</div>
-                <div>Prénom : </div>
-                <div>Adresse mail : </div>
+                <div>GUETTA</div>
+                <div>David</div>
+                <div>Informatique M2 </div>
             </div>
 
-            <div className={"rounded-lg pb-2 bg-gray-200 m-8 "}>
+            <div className={"rounded-lg pb-2 bg-gray-200 mx-24 "}>
                 <Link href="/PageBadgesCompteDeco">
-                <div className={" border-2 rounded-full p-4 text-center text-Text text-2xl pt-4 text-orange-500"}> DECONNEXION</div>
+                <h1 className={"font-bold border rounded-full text-center text-Text text-md py-1.5 mt-7"}> SE DECONNECTER</h1>
                 </Link>
             </div>
-            <div className={"rounded-lg pb-2 bg-gray-200 m-8 "}>
+            <div className={"bg-gray-200 mt-64"}>
                 <Link href="/PageBadgesCompteSupprime">
-                    <div className={" border-2 rounded-full p-4 text-center text-Text text-2xl pt-4 text-orange-500"}> SUPPRIMER</div>
+                    <div className={"p-4 text-center text-Text text-md pt-4 text-orange-500 underline decoration-1 underline-offset-2"}> SUPPRIMER SON COMPTE</div>
                 </Link>
-            </div>
-
-            <div className={"rounded-lg  bg-gray-200 m-8 "}>
-                <Link href="/PageBadgesProfilBadges">
-                <div className={"text-Text text-center text-Text text-2xl text-orange-500 "}>ANNULER</div>
-                </Link>
+                <div className={"rounded-lg  bg-gray-200"}>
+                    <div className={"text-Text text-center text-Text text-xs"}>Mentions légales</div>
+                </div>
             </div>
 
 
-            <div className={"rounded-lg  bg-gray-200"}>
-                <div className={"text-Text text-center text-Text text-xs text-orange-500 "}>Mentions légales</div>
-            </div>
+
 
         </div>
     )
