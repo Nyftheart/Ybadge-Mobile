d8231fc6b52219f4aaf09d1305ec033e42db3bd0
Aubin <aolivrie77@gmail.com>
2023-03-22 16:52:20 +0100

    [FIX] Page détails badge

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -2,40 +2,76 @@ import { useFrame, useLoader, Canvas } from '@react-three/fiber'
 
 import { Center, Text3D } from '@react-three/drei'
 import { Euler, MeshPhongMaterial } from 'three'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Motion, spring, PlainStyle } from 'react-motion'
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { BasicShadowMap } from 'three'
 
-export default function Badge({ initialPosition, objectFolderPath, animated = false }: any) {
+export default function Badge({
+  initialPosition,
+  objectFolderPath,
+  isLocked = false,
+  animated = false
+}: any) {
   const [isMouseDown, setIsMouseDown] = useState(false)
+  const [newObjectFolderPath, setNewObjectFolderPath] =
+    useState(objectFolderPath)
+
+  useEffect(() => {
+    if (isLocked) setNewObjectFolderPath(`${objectFolderPath}_locked`)
+  })
 
   return (
     <Canvas
       onTouchEnd={() => setIsMouseDown(false)}
       onPointerUp={() => setIsMouseDown(false)}
       shadows={{ type: BasicShadowMap }}
-      camera={{ position: [0, 0, 6.5] }}>
-      <directionalLight color={[255, 255, 255]} intensity={0.01} position={[-10, 20, 25]} />
-      <hemisphereLight color={0xffffff} groundColor={0x000000} position={[0, 20, 0]} />
-      <BadgeModel setIsMouseDown={setIsMouseDown} isMouseDown={isMouseDown} initialPosition={initialPosition} objectFolderPath={objectFolderPath} animated={animated} />
+      camera={{ position: [0, 0, 6.5] }}
+    >
+      <directionalLight
+        color={[255, 255, 255]}
+        intensity={0.01}
+        position={[-10, 20, 25]}
+      />
+      <hemisphereLight
+        color={0xffffff}
+        groundColor={0x000000}
+        position={[0, 20, 0]}
+      />
+      <BadgeModel
+        setIsMouseDown={setIsMouseDown}
+        isMouseDown={isMouseDown}
+        initialPosition={initialPosition}
+        objectFolderPath={newObjectFolderPath}
+        animated={animated}
+      />
     </Canvas>
   )
 }
 
-function BadgeModel({ isMouseDown, initialPosition, objectFolderPath, animated = false, setIsMouseDown = null }: any) {
+function BadgeModel({
+  isMouseDown,
+  initialPosition,
+  objectFolderPath,
+  animated = false,
+  setIsMouseDown = null
+}: any) {
   const [position, setPosition] = useState(initialPosition)
   const [rotation, setRotation] = useState({ x: 0, y: 0, z: 0 })
   const [pointer, setPointer] = useState({ x: 0, y: 0 })
   const [badgeIsTurned, setBadgeIsTurned] = useState(false)
 
   const materials = useLoader(MTLLoader, `${objectFolderPath}/material.mtl`)
-  const object = useLoader(OBJLoader, `${objectFolderPath}/object.obj`, (loader: any) => {
-    materials.preload()
-    loader.setMaterials(materials)
-  })
-  object.scale.set(0.3, 0.3, 0.3)
+  const object = useLoader(
+    OBJLoader,
+    `${objectFolderPath}/object.obj`,
+    (loader: any) => {
+      materials.preload()
+      loader.setMaterials(materials)
+    }
+  )
+  object.scale.set(2, 2, 2)
 
   useFrame(({ pointer: { x, y } }) => {
     if (animated) setPointer({ x, y: y - 0.75 })
@@ -47,7 +83,8 @@ function BadgeModel({ isMouseDown, initialPosition, objectFolderPath, animated =
     setRotation({ x: 0, y: rotation.y + Math.PI, z: 0 })
   }
 
-  const interpolate = (interpolated: PlainStyle) => new Euler(interpolated.x, interpolated.y, interpolated.z)
+  const interpolate = (interpolated: PlainStyle) =>
+    new Euler(interpolated.x, interpolated.y, interpolated.z)
 
   if (animated) {
     return (
@@ -65,7 +102,8 @@ function BadgeModel({ isMouseDown, initialPosition, objectFolderPath, animated =
                 y: spring(rotation.y),
                 z: spring(rotation.z)
               }
-        }>
+        }
+      >
         {(interpolated) => (
           <group
             onPointerDown={() => {
@@ -73,7 +111,8 @@ function BadgeModel({ isMouseDown, initialPosition, objectFolderPath, animated =
             }}
             onDoubleClick={() => turnBadge()}
             position={position}
-            rotation={interpolate(interpolated)}>
+            rotation={interpolate(interpolated)}
+          >
             {/* <Center position={[0, 0, -0.275]}>
               <Text3D
                 font={'/assets/three/montserrat.json'}
@@ -110,9 +149,14 @@ function BadgeModel({ isMouseDown, initialPosition, objectFolderPath, animated =
           x: spring(rotation.x),
           y: spring(rotation.y),
           z: spring(rotation.z)
-        }}>
+        }}
+      >
         {(interpolated) => (
-          <group onDoubleClick={() => turnBadge()} position={position} rotation={interpolate(interpolated)}>
+          <group
+            onDoubleClick={() => turnBadge()}
+            position={position}
+            rotation={interpolate(interpolated)}
+          >
             <Center position={[0, 0, -0.275]}>
               <Text3D
                 font={'/assets/three/montserrat.json'}
@@ -127,8 +171,13 @@ function BadgeModel({ isMouseDown, initialPosition, objectFolderPath, animated =
                   new MeshPhongMaterial({ color: 0x000000 }), // front
                   new MeshPhongMaterial({ color: 0x000000 }) // side
                 ]}
-                rotation-y={Math.PI}>
-                {new Date().getDate() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear()}
+                rotation-y={Math.PI}
+              >
+                {new Date().getDate() +
+                  '-' +
+                  (new Date().getMonth() + 1) +
+                  '-' +
+                  new Date().getFullYear()}
               </Text3D>
             </Center>
             <primitive object={object} />
--- pages/PageBadgesDefinition.tsx
+++ pages/PageBadgesDefinition.tsx
@@ -1,27 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Badge from '../components/Badge'
+import { Vector3 } from 'three'
 
 export default function PageBadgesDefinition() {
+  const [isLocked, setIsLocked] = useState(false)
+  const [objectFolderPath, setObjectFolderPath] = useState(
+    '/assets/three/badges/logo'
+  )
+
   return (
     <div className={'bg-Background min-h-screen pt-20'}>
-      <Link className="relative z-0 select-all" href="/PageBadges">
-        <img src="/arrow.png" width={21} height={14} alt="logo" className="ml-6 z-50" />
+      <Link className='relative z-0 select-all' href='/PageBadges'>
+        <img
+          src='/arrow.png'
+          width={21}
+          height={14}
+          alt='logo'
+          className='ml-6 z-50'
+        />
       </Link>
 
-      <div className="h-96  mb-10">
-        <div className="fixed z-40 h-screen w-screen touch-none">
+      <div className='h-96 mb-10'>
+        <div className='fixed z-40 h-screen w-screen touch-none'>
           <Badge
-            initialPosition={[0, 0, 0]}
-            objectFolderPath={'/assets/three/badges/logo_locked'}
-            animated={true}
+            initialPosition={new Vector3(0, 2, -10)}
+            objectFolderPath={objectFolderPath}
+            animated={isLocked ? false : true}
+            isLocked={isLocked}
           />
         </div>
       </div>
 
-      <div className={'select-none text-Text text-center text-red-600 text-2xl p-8 w-full SemiboldChill'}>Badges TEST</div>
+      <div
+        className={
+          'select-none text-Text text-center text-red-600 text-2xl p-8 w-full SemiboldChill'
+        }
+      >
+        Badges TEST
+      </div>
 
-      <p className={'select-none text-Text text-center text-orange-500 w-full text-sm font-light font-poppins'}> Ce badge correspond à ta filière. </p>
+      <p
+        className={
+          'select-none text-Text text-center text-orange-500 w-full text-sm font-light font-poppins'
+        }
+      >
+        Ce badge correspond à ta filière.
+      </p>
     </div>
   )
 }
