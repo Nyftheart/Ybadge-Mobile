39f3d8cd91e366b18ef56388501fd144bc21302c
NkwamoMaeva <nkwamomaeva@gmail.com>
2023-11-22 16:42:48 +0100

    chore(theme): create projet theme properties

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,3 +1,12 @@
+import { Poppins } from '@next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
 export default function Layout({ children }: any) {
-  return <>{children}</>
+  return <main className={poppins.className}>{children}</main>
 }
---
+++ src/utils/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles";
+import { Poppins } from '@next/font/google'
+
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+export const theme = createTheme({
+ palette: {
+    mode: 'dark',
+    primary: {
+      main:'#fff'
+    }
+ },
+ typography: {
+    fontFamily: poppins.style.fontFamily
+  },
+});
