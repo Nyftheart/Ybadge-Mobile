35ec123b3f6fd47dedbaf151bf9da49be020998e
Aubin Olivrie <aubin.olivrie@smice.com>
2023-11-22 18:06:08 +0100

    [ADD] Navbar first proto

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,27 +1,63 @@
-import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Navbar() {
-  return (
-    <div className="bg-menu fixed h-20 -bottom-1 z-0 max-w-md w-full flex justify-around items-center">
-      <Link href="/home">
-        <img
-          src="/assets/img/Recherche_Logo_Ybadges.png"
-          width={40}
-          height={40}
-          alt="logo"
-          className="m-auto"
-        />
-      </Link>
+  const [space, setSpace] = useState(13)
 
-      <Link href="/account">
-        <img
-          src="/assets/img/DefaultProfile.png"
-          width={28}
-          height={40}
-          alt="logo"
-          className="m-auto opacity-30"
-        />
-      </Link>
+  return (
+    <div className="fixed z-40 bottom-7 h-14 mx-8 w-80 bg-text rounded-2xl">
+      <div className="relative w-full h-full">
+        <div
+          id="front_items"
+          className="flex absolute z-20 w-20 h-20 -translate-y-3 rounded-full overflow-hidden bg-purple-500 transition-all duration-1000"
+          style={{ left: space }}
+        >
+          <div
+            className="min-w-[20rem] items-center justify-around flex transition-all duration-1000"
+            style={{ marginLeft: -space }}
+          >
+            <div>
+              <img src="/assets/img/logo.png" className="w-16 h-16" />
+            </div>
+
+            <div>
+              <img src="/assets/img/logo.png" className="w-16 h-16" />
+            </div>
+
+            <div>
+              <img src="/assets/img/logo.png" className="w-16 h-16" />
+            </div>
+          </div>
+        </div>
+
+        <div
+          id="back_items"
+          className="flex justify-around w-full items-center h-full"
+        >
+          <div>
+            <img
+              src="/assets/img/logo_locked.png"
+              onClick={() => setSpace(13)}
+              className="w-8"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/img/logo_locked.png"
+              onClick={() => setSpace(120)}
+              className="w-8"
+            />
+          </div>
+
+          <div>
+            <img
+              src="/assets/img/logo_locked.png"
+              onClick={() => setSpace(227)}
+              className="w-8"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,16 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    ...defaultTheme,
-    colors: {
-      unselect: 'rgba(255, 255, 255, 0.35)',
-      menu: '#414141',
-      background: '#131313',
-      text: '#ffffff',
-      transparent: 'rgba(255,255,255,0)',
-      search: 'rgba(65, 65, 65, 1)',
+    extend: {
+      ...defaultTheme,
+      colors: {
+        unselect: 'rgba(255, 255, 255, 0.35)',
+        menu: '#414141',
+        background: '#131313',
+        text: '#ffffff',
+        transparent: 'rgba(255,255,255,0)',
+        search: 'rgba(65, 65, 65, 1)',
+      },
     },
   },
   plugins: [plugin],
