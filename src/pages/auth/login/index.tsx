a111396cfbcfb5825afac19e92732738e945a8a9
NkwamoMaeva <nkwamomaeva@gmail.com>
2023-11-22 16:44:12 +0100

    fix(authentification): update login and register pages

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,57 +1,96 @@
-import Link from 'next/link'
+import Layout from '../layout'
+import * as React from 'react'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import Input from '@mui/material/Input'
+import Link from '@mui/material/Link'
+import InputLabel from '@mui/material/InputLabel'
+import InputAdornment from '@mui/material/InputAdornment'
+import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import VisibilityOutlined from '@mui/icons-material/VisibilityOutlined'
+import VisibilityOffOutlined from '@mui/icons-material/VisibilityOffOutlined'
 
 export default function Login() {
+  const [showPassword, setShowPassword] = React.useState(false)
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show)
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault()
+  }
   return (
-    <div className="pt-5 bg-background min-h-screen">
-      <img
-        src="/assets/img/LOGO_TYPO_BLANC.png"
-        width={125}
-        height={166}
-        alt="logo"
-        className="m-auto py-20"
-      />
+    <Layout>
       <div>
-        <div className="justify-around flex mx-8">
-          <Link href="/Register">
-            <p className="font-poppins text-md text-unselect">INSCRIVEZ-VOUS</p>
+        <TextField
+          fullWidth
+          id="email"
+          label="Adresse mail (Ynov)"
+          variant="standard"
+          margin="dense"
+          className="text-sm"
+        />
+
+        <FormControl
+          fullWidth
+          variant="standard"
+          margin="normal"
+          className="mt-3 text-sm"
+        >
+          <InputLabel htmlFor="standard-adornment-password">
+            Password
+          </InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? (
+                    <VisibilityOffOutlined />
+                  ) : (
+                    <VisibilityOutlined />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <div className="flex flex-row justify-between items-center mt-[-10px]">
+          <FormControlLabel
+            control={<Checkbox size="small" />}
+            label={<span className="text-xs">Se souvenir de moi</span>}
+          />
+
+          <Link href="#" underline="always" className="text-xs font-light">
+            Mot de passe oublié?
           </Link>
-          <p className="font-poppins font-medium text-md text-text">
-            SE CONNECTER
-          </p>
         </div>
-        <div className="relative z-0 mx-14 mt-8">
-          <input
-            type="text"
-            name=""
-            className="peer block w-full appearance-none rounded-b-none border-0 border-b border-text bg-transparent py-2.5 px-0 text-sm text-text focus:outline-none focus:ring-0 "
-            placeholder=" "
-          />
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-text duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 font-poppins font-light">
-            Adresse mail (Ynov)
-          </label>
+        <div className="mt-10 flex flex-col mx-auto items-center w-min">
+          <Button
+            className="rounded-full px-14 font-bold"
+            variant="outlined"
+            href="#outlined-buttons"
+          >
+            Connexion
+          </Button>
+          <Link
+            href="/auth/register"
+            underline="always"
+            className="text-xs font-light my-2"
+          >
+            Pas encore inscrit?
+          </Link>
         </div>
-        <div className="relative z-0 mx-14 mt-8">
-          <input
-            type="text"
-            name=""
-            className="peer block w-full appearance-none rounded-b-none border-0 border-b border-text bg-transparent py-2.5 px-0 text-sm text-text focus:outline-none focus:ring-0"
-            placeholder=" "
-          />
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-text duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 font-poppins font-light">
-            Mot de passe
-          </label>
-        </div>
-        <Link href="/PageBadges">
-          <div>
-            <button
-              type="submit"
-              className="flex mt-10 rounded-full bg-background border border-text px-14 py-2 text-text mx-auto font-poppins font-bold"
-            >
-              CONNEXION
-            </button>
-          </div>
-        </Link>
       </div>
-    </div>
+    </Layout>
   )
 }
