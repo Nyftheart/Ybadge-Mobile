63390880b8070e98d1a77a44faa962321d407617
NkwamoMaeva <nkwamomaeva@gmail.com>
2023-11-22 16:44:12 +0100

    fix(authentification): update login and register pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,28 @@
 import '../../public/assets/styles/globals.css'
-import type { AppProps } from 'next/app'
 import { SplashScreen } from '../components/splashScreen'
 import Layout from '../layouts/Layout'
+import React from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { theme } from '../utils/theme'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props
+
   return (
-    <div className="w-full h-full overflow-auto fixed bg-background">
-      <div className="max-w-md relative mx-auto">
-        <SplashScreen />
+    <ThemeProvider theme={theme}>
+      {
+        <div className="w-full h-full fixed px-6 bg-[url('/assets/img/background/bg.jpg')] bg-cover">
+          <div className="max-w-md h-full relative mx-auto">
+            <SplashScreen duration={2500} />
 
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </div>
-    </div>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </div>
+        </div>
+      }
+      <CssBaseline />
+    </ThemeProvider>
   )
 }
