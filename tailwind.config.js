b14bbfae26b9fc1c45c8f30de3a35c3429ee6e13
Aubin Olivrie <aubin.olivrie@smice.com>
2023-12-20 13:51:56 +0100

    [ADD] Custom fonts in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,20 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    ...defaultTheme,
-    colors: {
-      unselect: 'rgba(255, 255, 255, 0.35)',
-      menu: '#414141',
-      background: '#131313',
-      text: '#ffffff',
-      transparent: 'rgba(255,255,255,0)',
-      search: 'rgba(65, 65, 65, 1)',
+    extend: {
+      ...defaultTheme,
+      colors: {
+        unselect: 'rgba(255, 255, 255, 0.35)',
+        menu: '#414141',
+        background: '#131313',
+        text: '#ffffff',
+        transparent: 'rgba(255,255,255,0)',
+        search: 'rgba(65, 65, 65, 1)',
+      },
+      fontFamily: {
+        'chillax': ['Chillax', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      }
     },
     typography: {
       fontFamily: [
