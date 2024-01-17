ef4f1b82f0f7b894ed43a13d793321f97a4245f2
Aubin Olivrie <aubin.olivrie@smice.com>
2024-01-17 12:10:40 +0100

    [FIX] Splash screen

--- src/components/splashScreen/index.tsx
+++ src/components/splashScreen/index.tsx
@@ -3,40 +3,36 @@ import { useState } from 'react'
 
 export const SplashScreen: React.FC = () => {
   const [showSplash, setShowSplash] = useState(true)
-  const [duration, setDuration] = useState(30000)
 
   return (
     <CSSTransition
       in={showSplash}
-      timeout={duration}
+      timeout={30000}
       classNames="splash-screen"
       unmountOnExit={true}
     >
       <div
         onClick={() => {
           setShowSplash(false)
-          setDuration(250)
         }}
         className="fixed flex justify-center items-center flex-col z-50 top-0 left-0 w-full h-full bg-background"
       >
         <video
           poster="/assets/img/splashscreen-poster.jpg"
-          className="max-w-md h-screen object-fill"
+          className="max-w-md h-screen object-cover"
           width={'100%'}
           onPlay={() => {
-            setDuration(30000)
+            setTimeout(() => {
+              setShowSplash(false)
+            }, 7500)
           }}
           onEnded={() => {
             setShowSplash(false)
-            setDuration(250)
           }}
           muted
           autoPlay
         >
-          <source
-            src="/assets/videos/SPLASH_SCREEN_CUT.mp4"
-            type="video/mp4"
-          />
+          <source src="/assets/videos/SPLASH_SCREEN_VVF.mp4" type="video/mp4" />
         </video>
         {/* <img
           src="/assets/img/LOGO_TYPO_BLANC.png"
