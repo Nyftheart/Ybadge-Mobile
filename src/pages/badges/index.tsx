2c05c1d3ccaa7263957fbf55d34c4c9c340a416c
Alley-eddine <alleybenhassine@gamil.com>
2024-03-20 09:57:35 +0100

    Bug Alley

--- src/pages/badges/index.tsx
+++ src/pages/badges/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import jsonBadges from '../../../public/temp-data/badges.json'
-
 const BadgeCard = ({ badge, height, width }: any) => {
   let href = `/PageBadgesDefinition?badgeName=${badge.name}`
   if (badge.locked === true) {
@@ -10,7 +9,7 @@ const BadgeCard = ({ badge, height, width }: any) => {
 
   let imgSrc = `/assets/img/${badge.name}.png`
   if (badge.locked === true) {
-    imgSrc = `/assets/img/${badge.name}_locked.png`
+    imgSrc = `/assets/img/**${badge.name}_locked.png`
   }
 
   return (
@@ -22,10 +21,31 @@ const BadgeCard = ({ badge, height, width }: any) => {
 
 export default function Badges() {
   const [badges, setBadges] = useState<any>([])
-
+  const [inputText, setInputText] = useState('')
   useEffect(() => {
     setBadges(jsonBadges.data)
   }, [])
+
+  const labelList = () => {
+    const badgeList = badges
+      .map((badge: any) => {
+        if (badge.name.includes(inputText)) {
+          return (
+            <BadgeCard key={badge.id} width={57} height={57} badge={badge} />
+          )
+        }
+      })
+      .filter((badge: any) => badge !== undefined)
+    console.log('🚀 ~ badgeList ~ badgeList:', badgeList)
+    if (badgeList.length === 0) {
+      return (
+        <Link href="https://docs.google.com/forms/d/1sBSiOYbaEqXdTRjQXNlFzoWZXXWMfj5Xe0tg-M1Q6V8/edit">
+          <button style={buttonStyle}>Une idée ?</button>
+        </Link>
+      )
+    }
+    return badgeList
+  }
 
   return (
     <div className=" min-h-screen ">
@@ -39,9 +59,21 @@ export default function Badges() {
       <div className=" items-center mx-6 mb-11">
         <form className=" flex items-center text-left justify-end">
           <h1 className=" absolute text-text left-7  text-2xl font-semibold SemiboldChill">
-            ALL COLLECTIONS
+            <img
+              src="/assets/img/filtre.png"
+              width={29}
+              height={29}
+              alt="logo"
+              className="m-auto"
+            />
           </h1>
-          <Link href="PageBadgesRecherche">
+          {/* <Link href="../badgeidee"> */}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+            }}
+          >
             <img
               src="/assets/img/Search.png"
               width={22}
@@ -49,11 +81,43 @@ export default function Badges() {
               alt="logo"
               className="m-auto"
             />
-          </Link>
+            <div>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: 200,
+                }}
+              >
+                <input
+                  type="text"
+                  value={inputText}
+                  onChange={(e) => setInputText(e.target.value)}
+                  placeholder="Rechercher un badge"
+                  className="input-search input-slide-in"
+                  id="visible"
+                />
+                {inputText.length === 0 ? null : (
+                  <div className="searchbox">
+                    {inputText.length > 0 ? (
+                      <div className="searchbox__result">{labelList()}</div>
+                    ) : null}
+                  </div>
+                )}
+              </div>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-start',
+                marginLeft: 15,
+              }}
+            ></div>
+          </div>
         </form>
       </div>
 
-      
       <div>
         <div className={'text-text mx-7 MediumChill'}>Badges Classiques</div>
         <div className={'flex flex-wrap mt-6 mx-7 gap-6'}>
@@ -92,4 +156,13 @@ export default function Badges() {
       </div>
     </div>
   )
+}
+const buttonStyle = {
+  padding: '10px 20px',
+  fontSize: '16px',
+  backgroundColor: '#8c52ff',
+  color: '#fff',
+  border: 'none',
+  borderRadius: '35px',
+  cursor: 'pointer',
 }
