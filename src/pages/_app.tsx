331663de2cf0d0d346ab6306026ee30dbdb5b58c
Alley-eddine <alleybenhassine@gamil.com>
2024-03-20 09:57:35 +0100

    Bug Alley

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,13 @@ import '../../public/assets/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SplashScreen } from '../components/splashScreen'
 import Layout from '../layouts/Layout'
+import '../pages/badges/index.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="w-full h-full overflow-auto fixed bg-background">
       <div className="max-w-md relative mx-auto">
-        <SplashScreen />
+        {/* <SplashScreen /> */}
 
         <Layout>
           <Component {...pageProps} />
